4151fa6d65d81145f3684b03304f9f206aa050bf
박수진 <64060916+sujin1018@users.noreply.github.com>
2024-04-25 07:24:07 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int max = Integer.MIN_VALUE;
+    int max_index = 0;
+
+    for (int i = 1; i < 10; i++) {
+      int a = Integer.parseInt(br.readLine());
+      if (a > max) {
+        max = a;
+        max_index = i;
+      }
+    }
+    System.out.println(max);
+    System.out.println(max_index);
+  }
+}
